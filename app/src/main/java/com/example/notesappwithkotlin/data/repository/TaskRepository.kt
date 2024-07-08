f463debe4a00e3ff6971f643cc7e8ad16f939d95
duyennguyen14082003 <duyennguyen14082003@gmail.com>
2024-07-08 13:34:32 +0700

    Add task listing feature

--- app/src/main/java/com/example/notesappwithkotlin/data/repository/TaskRepository.kt
+++ app/src/main/java/com/example/notesappwithkotlin/data/repository/TaskRepository.kt
@@ -1,8 +1,10 @@
 package com.example.notesappwithkotlin.data.repository
 
 import com.example.notesappwithkotlin.data.model.Task
+import com.example.notesappwithkotlin.data.model.User
 import com.example.notesappwithkotlin.util.UiState
 
 interface TaskRepository {
     fun addTask(task: Task, result: (UiState<Pair<Task,String>>) -> Unit)
+    fun getTasks(user: User?, result: (UiState<List<Task>>) -> Unit)
 }
